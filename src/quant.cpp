7f9bff415739738bd009705c9cdec34a204c5d9f
Artem <munexrar@gmail.com>
2026-02-23 10:17:26 +0200

    changed-ctyle-to-cpp-style

--- src/quant.cpp
+++ src/quant.cpp
@@ -1,17 +1,15 @@
 #include "quant.h"
 #include <cmath>
 
-#define BLOCK_SIZE 64
 
 void quantize_block_64(float block[64], float scale) {
-    int i;
     if (scale <= 0.0f) return;
-    for (i = 0; i < BLOCK_SIZE; i++)
-        block[i] = (float)(int)roundf(block[i] / scale);
+    for (int i = 0; i < 64; ++i)
+        block[i] = std::roundf(block[i] / scale);
 }
 
 void dequantize_block_64(float block[64], float scale) {
     int i;
-    for (i = 0; i < BLOCK_SIZE; i++)
+    for (i = 0; i < 64; ++i)
         block[i] *= scale;
 }
